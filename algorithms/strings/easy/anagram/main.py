5bb971742d66425eac8d98b0481b444c2ecb3b5a
hamologist <hamologist@gmail.com>
2016-03-20 23:03:35 -0400

    Add minor optimizations to anagram code

--- algorithms/strings/easy/anagram/main.py
+++ algorithms/strings/easy/anagram/main.py
@@ -1,25 +1,21 @@
 import fileinput
+from collections import Counter
 
 lines = [list(line.strip()) for line in fileinput.input()]
 lines.pop(0)
 
 for line in lines:
-    not_in_count = 0
-    large_count = 0
+    count = 0
     if len(line) % 2 == 1:
         print(-1)
         continue
-    left = dict()
-    right = dict()
-    for x in range(0, int(len(line)/2)):
-        left_char = line[x]
-        right_char = line[len(line)-x-1]
-        left[left_char] = 1 if left_char not in left else left[left_char] + 1
-        right[right_char] = 1 if right_char not in right else right[right_char] + 1
+    half_len = int(len(line)/2)
+    left = Counter(line[:half_len])
+    right = Counter(line[half_len:])
 
     for x in left:
         if x not in right:
-            not_in_count += left[x]
+            count += left[x]
         elif left[x] > right[x]:
-            large_count += abs(left[x] - right[x])
-    print(not_in_count + large_count)
+            count += left[x] - right[x]
+    print(count)
